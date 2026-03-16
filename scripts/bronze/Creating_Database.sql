4d4b3eaf4b9cd1f89dfc7e375b3ce46daf01e76a
Geu Ghai Kuei <geukuei88@gmail.com>
2026-03-16 15:39:23 +0300

    Create Creating_Database.sql

---
+++ scripts/bronze/Creating_Database.sql
@@ -0,0 +1,46 @@
+/*      
+=======================================================
+
+Create Database and Schemas
+
+=======================================================
+Script Purpose:
+	This script creates a new database called 'DataWarehouse' after checking if it already exists.
+	If the database exists, it is dropped and recreated. Moreover, the cript sets up three schemas 
+	within the database and these are: 'bronze', 'silver' and 'gold'.
+
+
+WARNING
+	Running this script will drop the entire 'DataWarehouse' database if it exists.
+	All data in the database will permanently be deleted. Be cautious and make sure you
+	have proper backups before running this script
+*/
+
+USE master;
+GO
+
+--Drop and recreate 'DataWarehouse' database
+
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN 
+	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE DataWarehouse;
+END;
+GO
+
+-- Create 'DataWarehouse' database
+
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+-- Create Schemas
+
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
